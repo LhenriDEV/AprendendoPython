f68ec5429a7f8bdb0739004963ba0de7ab24d73d
lhenridev <lh.henri78@gmail.com>
2025-06-27 18:56:13 -0300

    Ex107 atualizado

--- exercicios/ex107/ex107.py
+++ exercicios/ex107/ex107.py
@@ -3,19 +3,21 @@ Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(),
 
 Adapte o código do desafio 107 criando uma função adicional chamada moeda() que consiga mostrar os números como um valor monetário formatado.
 
-# TODO: Modifique as funções que form criadas no desafio 107 para que elas aceitem um parâmetro a mais, informando se o valor retornado por elas vai ser ou não formatado pela função moeda(), desenvolvida no desafio 108.
+# TODO: Modifique as funções que foram criadas no desafio 107 para que elas aceitem um parâmetro a mais, informando se o valor retornado por elas vai ser ou não formatado pela função moeda(), desenvolvida no desafio 108.
 '''
 
 from modulos import moeda
 
 preco = float(input("Digite o preço: R$"))
 
-dobro = moeda.dobro(preco)
-metade = moeda.metade(preco)
-aumento = moeda.aumentar(preco, 10)
-desconto = moeda.diminuir(preco, 50)
+# (f de formatado)
+f_preco = moeda.moeda(preco)
+f_dobro = moeda.dobro(preco, f=True)
+f_metade = moeda.metade(preco, f=True)
+f_aumento = moeda.aumentar(preco, 10, f=True)
+f_desconto = moeda.diminuir(preco, 50, f=True)
 
-print(f"A metade de {moeda.moeda(preco)} é {moeda.moeda(metade)}")
-print(f"O dobro de {moeda.moeda(preco)} é {moeda.moeda(dobro)}")
-print(f"Aumentando 10% de {moeda.moeda(preco)}, temos {moeda.moeda(aumento)}")
-print(f"Diminuindo 50% de {moeda.moeda(preco)}, temos {moeda.moeda(desconto)}")
+print(f"A metade de {f_preco} é {f_metade}")
+print(f"O dobro de {f_preco} é {f_dobro}")
+print(f"Aumentando 10% de {f_preco}, temos {f_aumento}")
+print(f"Diminuindo 50% de {f_preco}, temos {f_desconto}")
--- exercicios/ex107/modulos/moeda.py
+++ exercicios/ex107/modulos/moeda.py
@@ -1,44 +1,128 @@
-def dobro(num: float | int) -> float | int:
+def dobro(num: float | int, f=False, f_moeda='R$') -> float | int:
     '''
-    Retorna o dobro de um número.
+    Retorna o dobro de um número. Se parâmetro 'f' for especificado, retorna string do número formatado.
+
+    **PARÂMETROS:**
+
+    > (float | int) num: Número ou preço.
+
+    > (bool) f: Formatação. Determina se o preço deve ser formatado como um valor monetário de acordo com sua moeda.
+
+    > (str) f_moeda: Formato de moeda a ser definido na formatação. Ex: "R$", "$", etc.
+
+    **RETORNA**
+
+    > (float | int): Caso f=False
+
+    > (str): Caso f=True
     '''
     res = num * 2
 
-    return res
+    if f:
+        # Formatado (retorna string)
+        return moeda(res, f_moeda)
+    else:
+        # Não formatado (retorna float/int)
+        return res
 
 
-def metade(num: float | int) -> float | int:
+def metade(num: float | int, f=False, f_moeda='R$') -> float | int:
     '''
     Retorna a metade de um número.
+
+     **PARÂMETROS:**
+
+    > (float | int) num: Número ou preço.
+
+    > (bool) f: Formatação. Determina se o preço deve ser formatado como um valor monetário de acordo com sua moeda.
+
+    > (str) f_moeda: Formato de moeda a ser definido na formatação. Ex: "R$", "$", etc.
+
+    **RETORNA**
+
+    > (float | int): Caso f=False
+
+    > (str): Caso f=True
     '''
     res = num / 2
 
-    return res
+    if f:
+        return moeda(res, f_moeda)
+    else:
+        return res
 
 
-def aumentar(num: float | int, por: int) -> float | int:
+def aumentar(num: float | int, por: int, f=False, f_moeda='R$') -> float | int:
     '''
     Aumenta em n% um número.
+
+     **PARÂMETROS:**
+
+    > (float | int) num: Número ou preço.
+
+    > (int) por: Porcentagem do valor. Ex: 10%
+
+    > (bool) f: Formatação. Determina se o preço deve ser formatado como um valor monetário de acordo com sua moeda.
+
+    > (str) f_moeda: Formato de moeda a ser definido na formatação. Ex: "R$", "$", etc.
+
+    **RETORNA**
+
+    > (float | int): Caso f=False
+
+    > (str): Caso f=True
     '''
 
     res = num + (num * (por / 100)) # Ex: 10% -> 0.10
 
-    return res
+    if f:
+        return moeda(res, f_moeda)
+    else:
+        return res
 
 
-def diminuir(num: float | int, por: int) -> float | int:
+def diminuir(num: float | int, por: int, f=False, f_moeda='R$') -> float | int:
     '''
     Desconta em n% um número.
+
+     **PARÂMETROS:**
+
+    > (float | int) num: Número ou preço.
+
+    > (int) por: Porcentagem do valor. Ex: 10%
+
+    > (bool) f: Formatação. Determina se o preço deve ser formatado como um valor monetário de acordo com sua moeda.
+
+    > (str) f_moeda: Formato de moeda a ser definido na formatação. Ex: "R$", "$", etc.
+
+    **RETORNA**
+
+    > (float | int): Caso f=False
+
+    > (str): Caso f=True
     '''
 
     res = num - (num * (por / 100))
 
-    return res
+    if f:
+        return moeda(res, f_moeda)
+    else:
+        return res
 
 
 def moeda(num: float, moeda="R$") -> str:
     '''
     Retorna string de um preço float com uma formatação monetária, incluindo cifrão.
+
+    **PARÂMETROS:**
+
+    > (float) num: Número ou preço.
+
+    > (str) moeda: Cifrão de moeda.
+
+    **RETORNA**
+
+    > (str)
     '''
 
     # Criar string do número, formatado da mesma forma que um valor monetário tradicional
@@ -53,3 +137,9 @@ if __name__ == '__main__':
     # Testando moeda()
     preco = float(input("Digite preço: R$"))
     print(f"ORIGINAL: {preco}\nFORMATADO: {moeda(preco, "$")}")
+
+    # Testando parâmetro f ---> utiliza moeda()
+    print(f"Dobro: {dobro(preco, f=True)}")
+    print(f"Metade: {metade(preco, f=True)}")
+    print(f"Aumentado em 20%: {aumentar(preco, 20, f=True)}")
+    print(f"Descontado em 50%: {diminuir(preco, 50, f=True)}")
