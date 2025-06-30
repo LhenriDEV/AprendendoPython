ec4fec56b6c4f89914829be84df2c26bf8c7a991
lhenridev <lh.henri78@gmail.com>
2025-06-30 17:03:05 -0300

    Ex107.py Reducao do codigo

--- exercicios/ex107/ex107.py
+++ exercicios/ex107/ex107.py
@@ -3,21 +3,12 @@ Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(),
 
 Adapte o código do desafio 107 criando uma função adicional chamada moeda() que consiga mostrar os números como um valor monetário formatado.
 
-# TODO: Modifique as funções que foram criadas no desafio 107 para que elas aceitem um parâmetro a mais, informando se o valor retornado por elas vai ser ou não formatado pela função moeda(), desenvolvida no desafio 108.
+Modifique as funções que foram criadas no desafio 107 para que elas aceitem um parâmetro a mais, informando se o valor retornado por elas vai ser ou não formatado pela função moeda(), desenvolvida no desafio 108.
+
+Adicione no módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(), que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui.
 '''
 
 from modulos import moeda
 
 preco = float(input("Digite o preço: R$"))
-
-# (f de formatado)
-f_preco = moeda.moeda(preco)
-f_dobro = moeda.dobro(preco, f=True)
-f_metade = moeda.metade(preco, f=True)
-f_aumento = moeda.aumentar(preco, 10, f=True)
-f_desconto = moeda.diminuir(preco, 50, f=True)
-
-print(f"A metade de {f_preco} é {f_metade}")
-print(f"O dobro de {f_preco} é {f_dobro}")
-print(f"Aumentando 10% de {f_preco}, temos {f_aumento}")
-print(f"Diminuindo 50% de {f_preco}, temos {f_desconto}")
+moeda.mostrar_resumo(preco, 80, 35)
--- exercicios/ex107/modulos/moeda.py
+++ exercicios/ex107/modulos/moeda.py
@@ -131,15 +131,53 @@ def moeda(num: float, moeda="R$") -> str:
     return num
 
 
+def mostrar_resumo(p, por_a=0, por_d=0):
+    '''
+    Imprime na tela um resumo formatado de variações de preço do valor informado: Dobro, metade, aumento e redução.
+
+    **PARÂMETROS**
+
+    > p: Preço/valor (float ou int)
+
+    > por_a: Porcentagem de aumento, por padrão 0 (int)
+
+    > por_d: Porcentagem de desconto, por padrão 0 (int)
+
+
+    **RETORNA**
+
+    > None
+    '''
+
+    print("-"*40)
+    print("RESUMO DO VALOR".center(40))
+    print("-"*40)
+
+    print(f"{'Preço analisado:'.ljust(25)} {moeda(p)}")
+    print(f"{'Dobro do preço:'.ljust(25)} {dobro(p, f=True)}")
+    print(f"{'Metade do preço:'.ljust(25)} {metade(p, f=True)}")
+    print(f"{f'Aumento em {por_a}%:'.ljust(25)} {aumentar(p, por_a, f=True)}")
+    print(f"{f'Redução em {por_d}%:'.ljust(25)} {diminuir(p, por_d, f=True)}")
+
+    print("-"*40)
+
 # Rode este bloco apenas se modulo for executado diretamente
 if __name__ == '__main__':
     
-    # Testando moeda()
     preco = float(input("Digite preço: R$"))
+    
+    '''
+    # Testando moeda()
     print(f"ORIGINAL: {preco}\nFORMATADO: {moeda(preco, "$")}")
+    '''
 
+    '''
     # Testando parâmetro f ---> utiliza moeda()
     print(f"Dobro: {dobro(preco, f=True)}")
     print(f"Metade: {metade(preco, f=True)}")
     print(f"Aumentado em 20%: {aumentar(preco, 20, f=True)}")
     print(f"Descontado em 50%: {diminuir(preco, 50, f=True)}")
+    '''
+
+    # Teste resumo()
+    mostrar_resumo(preco, 60, 50)
